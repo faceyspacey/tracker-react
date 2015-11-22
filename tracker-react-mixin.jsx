7757d1d060ce735c1ff0d26c87124ea3796a434b
James Gillmore <james@faceyspacey.com>
2015-11-22 12:56:34 -0800

    improved and consolidated `_.isHelper()` logic

--- tracker-react-mixin.jsx
+++ tracker-react-mixin.jsx
@@ -2,52 +2,52 @@ TrackerReact = {}; //so server side rendering isn't broken
 if(Meteor.isServer) return
 
 TrackerReact = {
-    componentWillMount() {
-        this.bindReactiveMethods(); //make all reactive bindings every time a component mounts
+	componentWillMount() {
+		this.bindReactiveMethods(); //make all reactive bindings every time a component mounts
 		this.assignEvents();
-        this._computations = {};
-    },
-    componentWillUnmount() {
-        _.each(this._computations, c => c.stop());
-        this._computations = null;
-    },
+		this._computations = {};
+	},
+	componentWillUnmount() {
+		_.each(this._computations, c => c.stop());
+		this._computations = null;
+	},
 
 	
-    bindReactiveMethods() {
-        let reg = /(^props|state|conext|getDOMNode|refs|events)$/; //`events` key will hold map of non-reactive event handlers
-
-        _.each(this, function(func, prop) {
-			//every other method on the component instance that doesn't start with `_` will be made reactive
-            if(this.hasOwnProperty(prop) && prop.charAt(0) !== '_' && _.isFunction(this[prop]) 
-				&& !TrackerReact.hasOwnProperty(prop) && !reg.test(prop)) {
-					
-                this.bindMethod(prop);
-            }
-        }, this);
-    },
-    bindMethod(prop) {
-        let oldFunction = this[prop];
-
-        this[prop] = function() {
-            if(this._computations[prop]) this._computations[prop].stop(); //there will be a computation every re-run except the 1st
-            let args = arguments,
-                response;
-
+	bindReactiveMethods() {
+		let reg = /(^props|state|conext|getDOMNode|refs|events|componentWillMount|componentDidMount|componentWillReceiveProps|shouldComponentUpdate|componentWillUpdate|componentDidUpdate|componentWillUnmount|getMeteorData|mixins|getInitialState|getDefaultProps)$/; //`events` key will hold map of non-reactive event handlers
+		
+		_.each(this, function(func, prop) {
+			if(this._isHelper(func, prop, reg)) this.bindMethod(prop);
+		}, this);
+	},
+	_isHelper(func, prop, reg) {
+		return this.hasOwnProperty(prop) 
+			&& prop.charAt(0) !== '_' //private methods
+			&& !reg.test(prop) //React component methods 
+			&& !TrackerReact.hasOwnProperty(prop) //methods from this mixin 
+			&& _.isFunction(this[prop]); //must be a function	
+	},
+	bindMethod(prop) {
+		let oldFunction = this[prop];
+	
+		this[prop] = function() {
+			if(this._computations[prop]) this._computations[prop].stop(); //there will be a computation every re-run except the 1st
+			let args = arguments,
+				response;
+		
 			//The magic behind this computation is it only ever runs once after its first run.
 			//When it does run that 2nd time, it's used just to force an update. The reactive function it wraps isn't even called.
 			//Then on the update, the cycle repeats, and the computation is stopped, and a new one is made. 
-            this._computations[prop] = Tracker.nonreactive(() => {
-                return Tracker.autorun(c => {
-                    if(c.firstRun) response = oldFunction.apply(this, args); 
-                    else this.forceUpdate();
-                });
-            });
-
-            return response;
-        }.bind(this);
+			this._computations[prop] = Tracker.nonreactive(() => {
+				return Tracker.autorun(c => {
+			    		if(c.firstRun) response = oldFunction.apply(this, args); 
+			    		else this.forceUpdate();
+				});
+			});
 		
-		window.MAIN = this;
-    },
+		    	return response;
+		}.bind(this);
+	},
 	
 	
 	//I'm opting to put events in a map, rather than helpers. In later versions, neither will need to be in nested maps
