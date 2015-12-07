90c973db503f3bca6ed5c56615bc20006bb2322e
James Gillmore <james@faceyspacey.com>
2015-12-07 11:33:27 -0800

    bump

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
 	name: "ultimatejs:tracker-react",
 	summary: "Helper-level reactive methods",
-	version: '0.0.5',
+	version: '0.0.6',
 	documentation: 'README.md',
 	git: 'https://github.com/ultimatejs/tracker-react'
 });
 
 Package.onUse(function (api) {
-	api.versionsFrom('METEOR@1.2.0.2');
+	api.versionsFrom('METEOR@1.2.1');
 	api.use('tracker');
 	api.use('underscore');
 	api.use('ecmascript@0.1.5');
