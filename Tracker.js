a30433def0540ffd3d11e8e31dd758bf18c02021
Dino Scheidt <github@dino-scheidt.de>
2016-02-26 03:12:48 +0100

    Add Decorator Support and JSDoc

--- Tracker.js
+++ Tracker.js
@@ -1,22 +1,28 @@
-// might as well abstract this pattern since something tells me we'll be using it a lot
-
+/**
+ * Create "one-time" reactive computations with Tracker (available as global)
+ * @param name {string} Component Reactive Data Property for Computation
+ * @param context {*} Target Component Instance
+ * @param dataFunc {*} Data Context
+ * @param updateFunc {*} Component ForceUpdate Method - To re-trigger render function
+ * @returns {*} Symbol(react.element) - Result data-element composition
+ */
 Tracker.once = function (name, context, dataFunc, updateFunc) {
   let data;
 
-  //stop it just in case the autorun never re-ran
+  // Stop it just in case the autorun never re-ran
   if (context[name] && !context[name].stopped) context[name].stop();
 
-  //NOTE: we may want to run this code in `setTimeout(func, 0)` so it doesn't impact the rendering phase at all
+  // NOTE: we may want to run this code in `setTimeout(func, 0)` so it doesn't impact the rendering phase at all
   context[name] = Tracker.nonreactive(() => {
     return Tracker.autorun(c => {
       if (c.firstRun) data = dataFunc.call(context);
       else {
 
-        //stop autorun here so rendering "phase" doesn't have extra work of also stopping autoruns; likely not too
+        // Stop autorun here so rendering "phase" doesn't have extra work of also stopping autoruns; likely not too
         // important though.
         if (context[name]) context[name].stop();
 
-        //where `forceUpdate` will be called in above implementation
+        // where `forceUpdate` will be called in above implementation
         updateFunc.call(context);
       }
     });
--- main.js
+++ main.js
@@ -1,9 +1,31 @@
+/**
+ * Tracker is available as a global variable but is extended for one time computations/invalidation.
+ * Implementation: See ./Tracker.js
+ */
 import Tracker from './Tracker';
 
+/**
+ * autorunRender(): The magic behind this computation is it only ever runs once after each time `render` is called. 
+ * When it does run that 2nd time, it's used just to force an update. The reactive function it wraps isn't even called.
+ * Then on the update, the cycle repeats, and the computation is stopped, and a new one is made.
+ * 
+ * Also, because the autorun is recreated on all React-triggered re-renders, any new code-paths possibly
+ * taken in `render` will automatically begin tracking reactive dependencies, thereby MERGING both models of reactivity:
+ * Meteor's various reactive data sources AND React's functional + unidirectional re-running of
+ * everything in component branches with state changes.
+ */
+
+/**
+ * Default. Provides a react component for inheritance as a clean alternative to mixins.
+ * Implementation:
+ *    "class MyApp extends TrackerReact(React.Component) { (...)"
+ * @param Component {*} React Component
+ */
 export default TrackerReact = function (Component) {
+  // No reactive computations needed for Server Side Rendering
   if (Meteor.isServer) return Component;
 
-  class TrackerReact extends Component {
+  class TrackerReactComponent extends Component {
 
     constructor(...args) {
       super(...args);
@@ -12,22 +34,16 @@ export default TrackerReact = function (Component) {
     }
 
     componentWillUnmount() {
+      // Implemented via ./Tracker.js
       this._renderComputation.stop();
       this._renderComputation = null;
     }
-
-    //The magic behind this computation is it only ever runs once after each time `render` is called.
-    //When it does run that 2nd time, it's used just to force an update. The reactive function it wraps isn't even called.
-    //Then on the update, the cycle repeats, and the computation is stopped, and a new one is made.
-    //Also, because the autorun is recreated on all React-triggered re-renders, any new code-paths possibly
-    //taken in `render` will automatically begin tracking reactive dependencies, thereby MERGING both models of reactivity:
-    //Meteor's various reactive data sources AND React's functional + unidirectional re-running of
-    //everything in component branches with state changes.
+    
     autorunRender() {
       let oldRender = this.render;
 
       this.render = () => {
-        // simple method we can offer in the `Meteor.Component` API
+        // Simple method we can offer in the `Meteor.Component` API
         return this.autorunOnce('_renderComputation', oldRender);
       };
     }
@@ -37,5 +53,40 @@ export default TrackerReact = function (Component) {
     }
   }
 
-  return TrackerReact;
+  return TrackerReactComponent;
+};
+
+
+/**
+ * Mixin. Use with ES7 / TypeScript Decorator or Mixin-Module.
+ * Implementation:
+ *   "@TrackerReactMixin
+ *    class MyApp extends React.Component { (...)"
+ * @type {{componentWillMount: (function()), componentWillUnmount: (function()), autorunRender: (function()), autorunOnce: (function(*=, *=))}}
+ */
+export const TrackerReactMixin = {
+  componentWillMount() {
+    // No reactive computations needed for Server Side Rendering
+    if(Meteor.isServer) return;
+
+    this.autorunRender();
+  },
+  componentWillUnmount() {
+    // No reactive computations needed for Server Side Rendering
+    if(Meteor.isServer) return;
+
+    this._renderComputation.stop();
+    this._renderComputation = null;
+  },
+  autorunRender() {
+    let oldRender = this.render;
+
+    this.render = () => {
+      // Simple method we can offer in the `Meteor.Component` API
+      return this.autorunOnce('_renderComputation', oldRender);
+    };
+  },
+  autorunOnce(name, dataFunc) {
+    return Tracker.once(name, this, dataFunc, this.forceUpdate);
+  }
 };
